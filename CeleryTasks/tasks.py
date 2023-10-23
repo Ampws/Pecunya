daa64b3133d5f8bd923a1be40a7554d2e478444e
DavidYordan <xinburuquan@gmail.com>
2023-10-24 06:26:07 +0800

    update

--- CeleryTasks/tasks.py
+++ CeleryTasks/tasks.py
@@ -8,18 +8,11 @@ logger = logging.getLogger(__name__)
 
 @celery_app.task()
 def listen_to_blockchain_task():
-    logger.info("test0")
     if os.name == 'nt':
-        logger.info('test1')
         loop = asyncio.new_event_loop()
-        logger.info('test2')
         asyncio.set_event_loop(loop)
-        logger.info('test3')
     else:
         loop = asyncio.get_event_loop()
     
-    logger.info("test4")
     listener = BlockchainListener()
-    logger.info("test5")
     loop.run_until_complete(listener.listen_to_blockchain())
-    logger.info("test6")
--- start_services.sh
+++ start_services.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 start_celery() {
-    celery -A Home worker --loglevel=info &
+    celery -A Home worker --loglevel=info --logfile=/logs/celery.log &
 }
 
 start_django() {
-    daphne Home.asgi:application -b 0.0.0.0 -p 8000
+    daphne Home.asgi:application -b 0.0.0.0 -p 8000 > /logs/daphne.log
 }
 
 start_celery
