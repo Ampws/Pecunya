ca5337870e2cfaaae7e3dbe9fc1a7c9dab45d98c
DavidYordan <xinburuquan@gmail.com>
2023-10-25 06:43:43 +0800

    update

--- start_services.sh
+++ start_services.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p /logs
+mkdir -p logs
 
 start_celery() {
-    touch /logs/celery.log
-    celery -A Home worker --loglevel=info --logfile=/logs/celery.log &
+    touch logs/celery.log
+    celery -A Home worker --loglevel=info --logfile=logs/celery.log &
 }
 
 start_django() {
-    daphne Home.asgi:application -b 0.0.0.0 -p 8000 > /logs/daphne.log
+    daphne Home.asgi:application -b 0.0.0.0 -p 8000 > logs/daphne.log
 }
 
 start_celery
